89b783042345fe65a411d49ff23ac6fa874a4784
Boston <BostonCEssary@gmail.com>
2023-08-22 15:47:10 -0700

    building flight search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,9 +3,6 @@ class Flight < ApplicationRecord
     belongs_to :arrival_airport, class_name: "Airport", inverse_of: :arriving_flights
 
 
-   def self.get_takeoff_dates
-        takeoffs = Flight.pluck(:takeoff)
-   end
 
    def takeoff_date_formatted
         takeoff.strftime("%m/%d/%Y")
