3e2ed3bdea0766a5e5849f3456d46340b5e7596b
Boston <BostonCEssary@gmail.com>
2023-08-23 10:53:36 -0700

    setting form params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,9 @@ class FlightsController < ApplicationController
         @departure_airports = Flight.all.map{ |f| [ f.departure_airport.code, f.departure_airport.id ]}
         @arrival_airports = Flight.all.map{ |f| [ f.arrival_airport.code, f.arrival_airport.id ]}
         
+        @date = params[:takeoff]
+
+        @flights = Flight.where({arrival_airport: params[:arrival_airport], departure_airport: params[:departure_airport]})
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root "flights#index"
+
+  get '/flights', to: 'flights#index'
 end
